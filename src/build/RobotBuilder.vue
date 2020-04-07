a33eea8ffc4e2445a3609805e964efa5deee9aa0
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-04-06 23:02:33 -0400

    Bug when trying to API call parts on component created

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="content">
+  <div v-if="availableParts" class="content">
       <div class="preview">
         <CollapsibleSection>
       <div class="preview-content">
@@ -51,34 +51,19 @@
         position="bottom"
         @partSelected="part => selectedRobot.base=part"/>
     </div>
-    <div>
-      <h1>Cart</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Robot</th>
-            <th class="cost">Cost</th>
-          </tr>
-        </thead>
-        <tbody>
-<tr v-for="(robot, index) in cart" :key='index'>
-<td>{{robot.head.title}}</td>
-<td class="cost">{{robot.cost}}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
   </div>
 </template>
 
 <script>
-import availableParts from '../data/parts';
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
 import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  created() {
+    this.$store.getParts();
+  },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
       next(true);
@@ -92,7 +77,6 @@ export default {
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
-      availableParts,
       addedToCart: false,
       cart: [],
       selectedRobot: {
@@ -106,8 +90,18 @@ export default {
   },
   mixins: [createdHookMixin],
   computed: {
+    availableParts() {
+      return this.$store.state.parts;
+    },
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale
+          ? '3px solid red'
+          : '3px solid #aaa',
+      };
     },
   },
   methods: {
@@ -119,9 +113,7 @@ export default {
       + robot.torso.cost
       + robot.rightArm.cost
       + robot.base.cost;
-      console.log(cost);
-      this.cart.push({ ...robot, cost });
-      console.log(this.cart);
+      this.$store.commit('addRobotToCart', { ...robot, cost });
       this.addedToCart = true;
     },
   },
@@ -238,15 +230,6 @@ export default {
   width: 210px;
   padding: 3px;
   font-size: 16px;
-}
-
-td, th {
-  text-align: left;
-  padding: 5px;
-  padding-right: 20px;
-}
-.cost{
-  text-align: right;
 }
 .sale-border{
   border: 3px solid red;
---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    cart: [],
+    parts: null,
+  },
+  actions: {
+    getParts() {
+      axios.get('/api/parts')
+        .then((response) => (response.data.bpi))
+        .catch((error) => console.log(error));
+    },
+  },
+  mutations: {
+    addRobotToCart(state, robot) {
+      state.cart.push(robot);
+    },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
+  },
+  getters: {
+    cartSaleItems(state) {
+      return state.cart.filter((item) => item.head.onSale);
+    },
+  },
+});
